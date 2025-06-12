9ee649e1d3644941ae1be57b07a94130f5fb1f97
Erica Silva <erycca20@gmail.com>
2025-06-11 22:09:17 -0300

    Continuação dia_4

---
+++ exercicios_aula/0_alturas_for.py
@@ -0,0 +1,13 @@
+# %%
+#
+soma = 0           # valor final
+qtde_entradas = 4  # o contador de entradas
+
+for i in range(qtde_entradas):
+    altura = input("Entre com a altura: ")
+    altura = float(altura)
+    soma += altura
+
+print("Soma das alturas:", soma)
+ 
+ 
