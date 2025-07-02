cd85f8bdba5ec46d39d9a56c135bf120fa6fb4e2
Erica Silva <erycca20@gmail.com>
2025-07-01 22:26:36 -0300

    Dia_6

--- exercicio_casa/2.18.py
+++ exercicio_casa/2.18.py
@@ -1,5 +1,5 @@
 # %%
-frases = {}
+dados = {}
 
 while True:
      frase = input("Entre com a frase:   ")
@@ -7,4 +7,24 @@ while True:
      if frase == "":
          break
      
+     if frase not in dados:
+          dados[frase] = 1
+     else:
+          dados[frase] += 1     
+
+for i in dados:    
+     print(i, "->", dados[i]) 
+
+items = list(dados.items())
+items.sort(key=lambda x:[-1], reverse=True)
+
+for i, j in items:
+     print(i, "->". j)
+
+
+
+
+     
+
+
      
